361fad0ef6cab83e25443b324b9dc5f4b8998dcd
TPGamesNL <pet.teun.03@gmail.com>
2024-05-22 17:11:39 +0200

    Add Docker and Python requirements installation to install script

--- azure/install.sh
+++ azure/install.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-echo abcd > /test.txt
+pwd > /test.txt
+ls > /test2.txt
 
+# Install Docker
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install -y python3 python3-pip python3-venv
+
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install Python
+apt-get update
+apt-get install -y python3 python3-pip
+
+pip install -r requirements.txt
+
+# TODO: create and start service for code
