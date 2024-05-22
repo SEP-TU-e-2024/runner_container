aed7254a777b4794f43054753b16e853b773ea65
ThomasNagel <tmj.nagel@gmail.com>
2024-05-22 15:53:53 +0200

    fix: added some comments

--- validator/validator.py
+++ validator/validator.py
@@ -1,7 +1,10 @@
+# This is a dummy validator
+# We will remove this and add the IOModule based implementation
 class Validator():
     def __init__(self):
         print("created")
 
+    #process submissions
     def submit(self, val):
         print(f'Got this value: {val}')
         with open("/results/results.txt", "+a") as f:
