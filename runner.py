a10193cdbae74b86b24f9eb4bffefe95e69a868b
Vlad Hategan <vhateg@gmail.com>
2024-05-27 10:55:59 +0200

    Started adding statistics tracking

--- runner.py
+++ runner.py
@@ -1,9 +1,10 @@
 import os
+import time
 
 import docker
 from docker.types import Mount
 
-from settings import DOCKER_FILE_PARRENT_DIR, DOCKER_RESULTS, DOCKER_SUBMISSION, DOCKER_VALIDATOR
+from settings import DOCKER_FILE_PARRENT_DIR, DOCKER_RESULTS, DOCKER_SUBMISSION, DOCKER_VALIDATOR, DOCKER_TIMEOUT, DOCKER_MEMORY, DOCKER_CPUS
 
 
 class Runner():
@@ -14,6 +15,7 @@ class Runner():
 
         self._config_mounts()
         self.container = self.docker_client.containers.create(image=self.docker_image, mounts=self.mounts, detach=True)
+        self._config_resources()
         #self.container = self.docker_client.containers.create(image=DOCKER_IMAGE, mounts=self.mounts, detach=True)
     
     def _config_mounts(self):
@@ -21,13 +23,21 @@ class Runner():
         self.mounts = [Mount(target=DOCKER_SUBMISSION, source=f'{cwd}{DOCKER_SUBMISSION}', type="bind", read_only=True),
                        Mount(target=DOCKER_VALIDATOR, source=f'{cwd}{DOCKER_VALIDATOR}', type="bind", read_only=True),
                        Mount(target=DOCKER_RESULTS, source=f'{cwd}{DOCKER_RESULTS}', type="bind", read_only=False)]
+        
+    def _config_resources(self):
+        self.container.update(mem_limit=DOCKER_MEMORY, cpus=DOCKER_CPUS)
 
     def run(self):
         print("Running...")
         self.container.start()
 
+        start_time = time.time()
         for data in self.container.logs(stream=True):
             print(f"{data.decode()}", end='')
+            if (time.time() - start_time) > DOCKER_TIMEOUT:
+                self.container.stop()
+                break
+
 
 #----------------------------------------------------------------
 # TESTING
--- settings.py
+++ settings.py
@@ -8,3 +8,8 @@ DOCKER_FILE_PARRENT_DIR = "." # just for testing
 DOCKER_SUBMISSION = "/submission"
 DOCKER_VALIDATOR = "/validator"
 DOCKER_RESULTS = "/results"
+
+# Time, Memory and CPU
+DOCKER_TIMEOUT = 10
+DOCKER_MEMORY = "512m"
+DOCKER_CPUS = 2
