7bbb97c0cf5898603e73d34717242aafe20bf3d8
TPGamesNL <pet.teun.03@gmail.com>
2024-05-30 11:54:18 +0200

    Add TODO note on entrypoint file

--- entrypoint.py
+++ entrypoint.py
@@ -3,6 +3,7 @@ import sys
 import time
 
 if __name__ == "__main__":
+    # TODO: replace with production code, this is a sample for testing purposes
     print("Hello there")
     time.sleep(10)
     sys.exit(123)
