49474f74064d02413adb9d3879a75532a65bc67a
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2024-06-07 14:06:02 +0200

    Added comments

--- protocol/counter.py
+++ protocol/counter.py
@@ -1,12 +1,23 @@
+"""
+This module contains the Counter class.
+"""
+
 import threading
 
 
 class Counter:
-    def __init__(self) -> None:
-        self.id = 0
+    """
+    The counter class can be used to generate unique messaage IDs by incrementing a counter.
+    """
+
+    def __init__(self, start: int = 0) -> None:
+        self.id = start
         self.lock = threading.Lock()
 
     def generate(self):
+        """
+        This method retruns the generted unique messaage ID by incrementing a counter. This method is thread-safe.
+        """
         with self.lock:
             current_id = self.id
             self.id += 1
