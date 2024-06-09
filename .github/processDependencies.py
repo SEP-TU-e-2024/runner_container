95e4590678ec971ca23104406ba628b071e79f29
ThomasNagel <tmj.nagel@gmail.com>
2024-06-09 20:57:14 +0200

    fixed the dependency issue

--- .github/processDependencies.py
+++ .github/processDependencies.py
@@ -4,14 +4,15 @@ import numpy as np
 # return a matrix
 def process_file(file):
     df = pd.read_csv(file)
-    cols = list(df.columns)
 
-    df = df.drop([index for index, row in df.iterrows() if row['Dependent File'] not in cols])
-    df = df[[col for col in df.columns if (col == df["Dependent File"]).any()]].dropna(how='all')
+    non_dep_files = list(df)[1:]
 
-    print('\nCyclic dependencies:\n', df)
+    df = df.drop([index for index, row in df.iterrows() if row['Dependent File'] not in non_dep_files])
+    df = df[[col for col in df.columns if (col == df["Dependent File"]).any()]]
 
-    return np.nan_to_num(df.to_numpy(), nan=0), list(df.columns)
+    print(df)
+
+    return df.fillna(0).to_numpy(dtype=int), list(df.columns)
 
 
 def compute_percentages_dependencies(matrix, columns, total_num_files):
@@ -21,7 +22,8 @@ def compute_percentages_dependencies(matrix, columns, total_num_files):
     for i in range(num_files):
         for j in range(num_files):
             if matrix[i][j] != 0 and matrix[j][i] != 0 and i != j:
-                print(f"Dependency violation found at position: ({i}, {j}) \t Files: {columns[i]}, {columns[j]}")
                 violations += 1
+                if i > j: # only print this message once
+                    print(f"Dependency violation found at position: ({i}, {j}) \t Files:\n{columns[i]}\n{columns[j]}\n")
 
     return {"Cyclic dependencies": violations / (2 * total_num_files)}
