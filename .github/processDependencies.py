64fbe02dcaadde27b39353e2d7b7dbb8511b92ac
ThomasNagel <tmj.nagel@gmail.com>
2024-06-10 00:23:44 +0200

    dfs search trough dependency graph

--- .github/processDependencies.py
+++ .github/processDependencies.py
@@ -14,16 +14,33 @@ def process_file(file):
 
     return df.fillna(0).to_numpy(dtype=int), list(df.columns)
 
+def find_cycles(matrix, not_visited, path, cycles):
+    N, _ = matrix.shape
+    not_visited.remove(path[-1])
 
-def compute_percentages_dependencies(matrix, columns, total_num_files):
-    num_files, _ = matrix.shape
-    violations = 0
+    for i in range(N):
+        if matrix[path[-1]][i]: # there is a dependency
+            if i in path: # cycle found
+                cycles.append(path.copy())
+            elif i in not_visited: # recurse
+                path.append(i)
+                find_cycles(matrix, not_visited, path, cycles)
+                path.pop()
+    
+    if len(path) == 1 and len(not_visited) != 0: # find a new starting point if graph is disconnected
+        find_cycles(matrix, not_visited, [next(iter(not_visited))], cycles)
 
-    for i in range(num_files):
-        for j in range(num_files):
-            if matrix[i][j] != 0 and matrix[j][i] != 0 and i != j:
-                violations += 1
-                if i > j: # only print this message once
-                    print(f"Dependency violation found at position: ({i}, {j}) \t Files:\n{columns[i]}\n{columns[j]}\n")
+def compute_percentages_dependencies(file, total_num_files):
+    matrix, cols = process_file(file)
 
-    return {"Cyclic dependencies": violations / (2 * total_num_files)}
+    N, _ = matrix.shape
+    cycles = list()
+    find_cycles(matrix, set([i for i in range(N)]), [0], cycles)
+
+    # Show which cycles have been found
+    for cycle in cycles:
+        print(f"\nA cycle of length {len(cycle)} has been found:")
+        for i in cycle:
+            print(f"\t{cols[i]}")
+
+    return {"Cyclic dependencies": len(cycles) / total_num_files}
