c4db0e1d8522b847310062255d54f7c3d91cafe2
Vlad Hategan <vhateg@gmail.com>
2024-06-10 15:45:21 +0200

    Nicefied

--- validator/IOModule.py
+++ validator/IOModule.py
@@ -2,10 +2,6 @@ import os
 
 import psutil
 
-import subprocess
-
-import re
-
 class IOModule:
     """
     This is a base class that should be used as a template for the Validator.
@@ -37,31 +33,16 @@ class IOModule:
         """
         pass
 
-    def __extract_VmPeak(info_string):
-        """
-        Function to extract the VmPeak value from the process status output.
-        """
-        pattern = r'VmPeak:\s*([0-9]+)\s+kB'
-        match = re.search(pattern, info_string)
-
-        if match:
-            return match.group(1)
-        else:
-            return None
-
     def __del__(self):
         """On termination of the program (when this object will be deleted) this will write all performance and score metrics to a file"""
 
         # get cpu times
         current_process = psutil.Process(os.getpid())
-        cpu_times = current_process.cpu_times();
-        cpu_times_sum = cpu_times.user + cpu_times.system + cpu_times.children_user + cpu_times.children_system;
+        cpu_times = current_process.cpu_times()
+        cpu_times_sum = cpu_times.user + cpu_times.system + cpu_times.children_user + cpu_times.children_system
 
-        # obtain MaxRSS from /proc/{pid}/status
-        proc_directory = '/proc/' + str(os.getpid()) + '/status'
-        result = subprocess.run(['cat', proc_directory], capture_output=True, text=True)
-        
-        max_memory = self.__extract_VmPeak(result.stdout)
+        # obtain MaxRSS
+        max_memory = ""
 
         # Write metrics to csv file
         metrics_file = open(self._OUTPUT_FILE, 'w', newline='')
