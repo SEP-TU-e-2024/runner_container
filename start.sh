8d8b3e2110bb58e6e13971a2031196e8e38e2a89
ThomasNagel <tmj.nagel@gmail.com>
2024-06-10 15:06:58 +0200

    updated start.sh, partually done

--- start.sh
+++ start.sh
@@ -1,12 +1,22 @@
 mkdir runenv
+mkdir unpack_dir
 
-cd runenv
+# unzip the submission code & find the directory that contains main
+# then we copy all contents from that directory to runenv
+unzip submission/submission.zip -d unpack_dir
+cp -r $(dirname $(find /unpack_dir -name "main.py" | head -n 1))/** /runenv
+rm -rf /unpack_dir/*
+
+# unzip all validator files & find the directory that contains the validator file
+# after we clean everything up
+unzip validator/validator.zip -d -o unpack_dir
+cp -r $(dirname $(find /unpack_dir -name "validator.py" | head -n 1)) /runenv
+rm -rf /unpack_dir
+
+cd /runenv
 pipenv shell
 
-pipenv install -r submission/requirements.txt
+pipenv install -r requirements.txt
 pipenv install -r validator/requirements.txt
-
-cp -r /submission/** ./
-cp -r /validator/** ./
 
 python main.py
