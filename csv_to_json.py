6a6c6d22c9ee57164d0c8d5abb3dc1aa16afe660
Vlad Hategan <vhateg@gmail.com>
2024-06-12 15:34:15 +0200

    Fixed paths

--- csv_to_json.py
+++ csv_to_json.py
@@ -9,13 +9,13 @@ def conversion():
     '''Function to convert files to json strings'''
 
     # convert results.scv from csv to a json string
-    with open(path.join(DOCKER_RESULTS, 'results.csv')) as file:
+    with open(path.join('results/results.csv')) as file:
         csv_reader = DictReader(file)
         data = list(csv_reader)
     results_string = dump(data)
 
     # convert metrics.scv from csv to a json string
-    with open(path.join(DOCKER_RESULTS, 'metrics.csv')) as file:
+    with open(path.join('results/metrics.csv')) as file:
         csv_reader = DictReader(file)
         data = list(csv_reader)
     metrics_string = dump(data)
