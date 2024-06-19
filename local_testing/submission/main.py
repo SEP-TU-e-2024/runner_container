ab0a0bc21c22405e156c323c33c68ac9e45a279f
TPGamesNL <pet.teun.03@gmail.com>
2024-06-19 14:11:08 +0200

    Remove sleep from default submission

--- local_testing/submission/main.py
+++ local_testing/submission/main.py
@@ -1,7 +1,5 @@
 # Dummy code for testing functionality of the IOModule
 
-from time import sleep
-
 from validator.validator import Validator
 
 io = Validator()
@@ -11,5 +9,3 @@ b = io.obtain_data("Enter another number: ")
 sum = int(a) + int(b)
 
 io.push_data(str(sum))
-
-sleep(5)
