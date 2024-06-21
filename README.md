a651f11a177378f3aea07e9bed87601d35b86c44
TPGamesNL <pet.teun.03@gmail.com>
2024-06-21 10:41:08 +0200

    Add .env file

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Runner Container
 This contains the library based implementation.
-The submitted code is run in the docker container and calls the library, which will also run in the container.
+The submitted code is run in the Docker container and calls the library, which will also run in the container.
 Then the library will write its results to a file.
+
+## `.env` setup
+```
+JUDGE_HOST = "localhost"
+JUDGE_PORT = "12345"
+JUDGE_CONNECTION_RETRY_WAIT = "5"
+```
+
+## Start Files
+To start the Judge Runner including the protocol, use `runner.py`. For development purposes, you can also run this stand-alone with `container.py`. Do make sure that you run a webserver in `local_testing` at the same time.
--- settings.py
+++ settings.py
@@ -2,21 +2,27 @@
 This module contains the settings for the runner.
 """
 
+import os
+
+import dotenv
+
+dotenv.load_dotenv()
+
 # ----------------------------------------------------------------
 # Connection
 # ----------------------------------------------------------------
 
-JUDGE_HOST = "10.1.0.6" # TODO: change to prod version, currently IP of the jumpbox, ask Teun for how to use it
+JUDGE_HOST = os.getenv("JUDGE_HOST", "localhost") # TODO: change to prod version, currently IP of the jumpbox, ask Teun for how to use it
 """
 Host of the the judge server.
 """
 
-JUDGE_PORT = 12345
+JUDGE_PORT = int(os.getenv("JUDGE_PORT", "12345"))
 """
 The port of the judge server.
 """
 
-RETRY_WAIT = 5
+RETRY_WAIT = int(os.getenv("JUDGE_CONNECTION_RETRY_WAIT", "5"))
 """
 Time in seconds to wait before retrying to connect to the judge server.
 """
