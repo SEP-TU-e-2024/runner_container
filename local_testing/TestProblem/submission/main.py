f452e28bd2e4d3549ae5afa8c0cc2080211184e9
TPGamesNL <pet.teun.03@gmail.com>
2024-06-21 13:40:55 +0200

    Improve local testing env

---
+++ local_testing/TestProblem/submission/main.py
@@ -0,0 +1,11 @@
+# Dummy code for testing functionality of the IOModule
+
+from validator.validator import Validator
+
+io = Validator()
+
+a = io.obtain_data("Enter a number: ")
+b = io.obtain_data("Enter another number: ")
+sum = int(a) + int(b)
+
+io.push_data(str(sum))
--- local_testing/run.sh
+++ local_testing/run.sh
@@ -1,2 +1,25 @@
-# You can run a simple webserve where the container can download the zip files from
+#!/bin/bash
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Use './run.sh <problem name>'"
+	exit 1
+fi
+
+cd $1
+
+# Zip the submission
+echo "Creating submission.zip"
+cd submission
+zip -r ../submission.zip ./*
+cd ..
+
+# Zip the validator
+echo "Creating validator.zip"
+cd validator
+zip -r ../validator.zip ./*
+zip -j ../validator.zip ../../IOModule.py
+cd ..
+
+# Start the web server
 python3 -m http.server 8001
